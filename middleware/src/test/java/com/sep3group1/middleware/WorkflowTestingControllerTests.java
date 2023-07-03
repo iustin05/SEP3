4b61777c2bd44de13cb600c00836bd318c7e3223
iustin05 <iusgrig@icloud.com>
2023-07-03 15:40:12 +0200

    Improved basic unit tests - ADDED unit test for Workflow Testing Controller - REMOVED default unit test

--- middleware/src/test/java/com/sep3group1/middleware/WorkflowTestingControllerTests.java
+++ middleware/src/test/java/com/sep3group1/middleware/WorkflowTestingControllerTests.java
@@ -2,9 +2,11 @@ package com.sep3group1.middleware;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 class WorkflowTestingControllerTests {
 
     @Autowired
