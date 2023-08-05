169046c4d9ae5505b5480f4a69a5fa8217515dc4
IonutAdriann <ionutrotaru077@gmail.com>
2023-08-05 19:36:35 +0300

    fix: changing the structure of the User class

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -10,8 +10,4 @@ public class User
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string? Role { get; set; }
-
-    public ICollection<Post>? Posts  { get; set; }
-    public ICollection<Bookmark>? Bookmarks { get; set; }
-
 }
