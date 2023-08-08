20959038976afebb7e648644f5c594cedae80c57
iustin05 <iusgrig@icloud.com>
2023-08-08 15:37:11 +0200

    Covered Null Use Case

--- backend/EFC/DbAccess.cs
+++ backend/EFC/DbAccess.cs
@@ -6,8 +6,8 @@ namespace backend.EFC;
 
 public class DbAccess : DbContext
 {
-    public DbSet<Car> Cars { get; set; }
+    public DbSet<Car>? Cars { get; set; }
     public DbSet<Booking>? Bookings { get; set; }
-    public DbSet<User> Users { get; set; }
+    public DbSet<User>? Users { get; set; }
     public DbSet<Rating>? Ratings { get; set; }
 }
