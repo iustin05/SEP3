5dfaaa4662da065640eef1f1e3fd6061fb7eaf65
iustin05 <iusgrig@icloud.com>
2023-08-08 14:55:15 +0200

    Added Missing Models

---
+++ backend/Models/Car.cs
@@ -0,0 +1,85 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models;
+
+public class Car
+{
+    [Key]
+    public required int Id { get; set; }
+        
+    [Required]
+    public required string Make { get; set; }
+        
+    [Required]
+    public required string Model { get; set; }
+        
+    [Required]
+    public required string Color { get; set; }
+        
+    [Required]
+    public required string Fuel { get; set; }
+        
+    [Required]
+    public required string Transmission { get; set; }
+        
+    [Required]
+    public required CarCategory Category { get; set; }
+        
+    [Required]
+    public required int Year { get; set; }
+        
+    [Required]
+    public required int Mileage { get; set; }
+        
+    [Required]
+    public required int PricePerDay { get; set; }
+        
+    [Required]
+    public required string Description { get; set; }
+        
+    [Required]
+    public required string Image { get; set; }
+        
+    [Required]
+    public required DateTime CreatedAt { get; set; }
+        
+    [Required]
+    public required DateTime UpdatedAt { get; set; }
+
+    [Required]
+    public required int NumberOfSeats { get; set; }
+
+    [Required]
+    public required int EuroNorm { get; set; }
+
+    [Required]
+    public required int HorsePower { get; set; }
+
+    [Required]
+    public required float FuelConsumption { get; set; }
+
+    [Required]
+    public required int Deposit { get; set; }
+
+    [Required]
+    public required CarStatus Status { get; set; }
+        
+    public Rating? Ratings { get; set; }
+
+    [Required]
+    public required DateTime CreatedTime { get; set; }
+}
+
+public enum CarCategory
+{
+    Economy,
+    Standard,
+    Luxury,
+}
+
+public enum CarStatus
+{
+    Available,
+    Unavailable,
+    InService,
+}
---
+++ backend/Models/Rating.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models;
+
+public class Rating
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(Car))]
+    public required int CarId { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(User))]
+    public required int UserId { get; set; }
+
+    [Required]
+    [MaxLength(500)]
+    [MinLength(5)]
+    [RegularExpression(@"^[a-zA-Z0-9\s]*$")]
+    public required string Comment { get; set; }
+
+    [Required]
+    public required DateTime CreatedTime { get; set; }
+
+    [Required]
+    [Range(1, 5)]
+    public int NumberOfStars { get; set; }
+}
